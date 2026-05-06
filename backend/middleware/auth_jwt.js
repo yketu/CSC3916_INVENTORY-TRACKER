99009f7d18b1a349b0643570d8376fc024bca938
yketu <157855738+yketu@users.noreply.github.com>
2026-05-06 12:04:57 -0600

    Refactor JWT authentication middleware for async handling

--- backend/middleware/auth_jwt.js
+++ backend/middleware/auth_jwt.js
@@ -1,21 +1,28 @@
-var passport = require('passport');
-var JwtStrategy = require('passport-jwt').Strategy;
-var ExtractJwt = require('passport-jwt').ExtractJwt;
-var User = require('../models/user');
+const passport = require('passport');
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
+const User = require('../models/user');
 
-var opts = {};
-opts.jwtFromRequest = ExtractJwt.fromAuthHeaderWithScheme("jwt");
-opts.secretOrKey = process.env.SECRET_KEY;
+const opts = {
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+  secretOrKey: process.env.SECRET_KEY
+};
 
-passport.use(new JwtStrategy(opts, function(jwt_payload, done) {
-    User.findById(jwt_payload.id, function (err, user) {
-        if (user) {
-            done(null, user);
-        } else {
-            done(null, false);
-        }
-    });
-}));
+passport.use(
+  new JwtStrategy(opts, async (jwt_payload, done) => {
+    try {
+      const user = await User.findById(jwt_payload.id);
 
-exports.isAuthenticated = passport.authenticate('jwt', { session : false });
-exports.secret = opts.secretOrKey ;
+      if (user) {
+        return done(null, user);
+      } else {
+        return done(null, false);
+      }
+
+    } catch (err) {
+      return done(err, false);
+    }
+  })
+);
+
+exports.isAuthenticated = passport.authenticate("jwt", { session: false });
