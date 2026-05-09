0c294bfafc9321232c287b9e9e38ee9e1f89827a
yketu <157855738+yketu@users.noreply.github.com>
2026-05-08 21:16:28 -0600

    Update axiosConfig.js

--- frontend/src/api/axiosConfig.js
+++ frontend/src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'https://inventory-backend-17fp.onrender.com',
+  baseURL: 'https://inventory-backend-17fp.onrender.com/api',
   headers: {
     'Content-Type': 'application/json',
   },
