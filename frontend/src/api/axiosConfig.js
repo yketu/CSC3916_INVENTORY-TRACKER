516a262b7de4985654e00b4d450c0b87d9a22f33
yketu <157855738+yketu@users.noreply.github.com>
2026-05-08 20:31:33 -0600

    Change axios base URL to production endpoint

--- frontend/src/api/axiosConfig.js
+++ frontend/src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5001/api',
+  baseURL: 'https://inventory-backend-17fp.onrender.com',
   headers: {
     'Content-Type': 'application/json',
   },
